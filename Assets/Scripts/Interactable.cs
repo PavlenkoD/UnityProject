7c3f9ffb31d51b08e9348919520aaab677407115
pavlenko_d <carboneum.d@gmail.com>
2020-03-12 15:19:00 +0300

    AddProjecFiles

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    public float radius = 3f;
+    public Transform interactionsTransform;
+
+    bool isFocus = false;
+    Transform player;
+
+    bool hasInteracted = false;
+
+    public virtual void Interact()
+    {
+        // Виртуальный метод взаимодействия
+        Debug.Log( "Interacting with" + transform.name );
+    }
+
+    private void Update()
+    {
+        if( isFocus && !hasInteracted)
+        {
+            float distance = Vector3.Distance( player.position , interactionsTransform.position );
+            if(distance <= radius)
+            {
+                Interact( );
+                hasInteracted = true;
+            }
+        }
+    }
+
+    public void OnFocused (Transform playerTransform )
+    {
+        isFocus = true;
+        player = playerTransform;
+        hasInteracted = false;
+    }
+
+    public void OnDefocused()
+    {
+        isFocus = false;
+        player = null;
+        hasInteracted = false;
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if( interactionsTransform == null )
+            interactionsTransform = transform;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere( interactionsTransform.position , radius );
+    }
+}
