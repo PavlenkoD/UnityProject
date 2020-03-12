cfe3093401306bcb5c0d12493ff9491a12f83b82
pavlenko_d <carboneum.d@gmail.com>
2020-03-12 16:02:25 +0300

    первая половина гайда

---
+++ Assets/EquipmentManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EquipmentManager : MonoBehaviour
+{
+        
+    #region Singleton
+
+    public static EquipmentManager instance;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    #endregion
+
+    Equipment[] currentEquipment;
+
+    private void Start()
+    {
+        int numSlots = System.Enum.GetNames( typeof( EquipmentSlot ) ).Length;
+        currentEquipment = new Equipment[ numSlots ];
+    }
+
+    public void Equip (Equipment newItem)
+    {
+        int slotIndex = ( int ) newItem.equipSlot;
+
+        currentEquipment[ slotIndex ] = newItem;
+    }
+
+}
